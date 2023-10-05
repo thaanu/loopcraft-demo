9b20361bb37f419d27777dd62d07a6464f5ba648
Shan <ahmed.shan@treetophospital.com>
2023-10-05 17:41:57 +0000

    Added routes
    - customer
    - order
    - payment

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -31,7 +34,6 @@ Route::post('products/create', [ProductsController::class, 'create'])->middlewar
 Route::put('products/{id}/update', [ProductsController::class, 'update'])->middleware('auth:sanctum');
 Route::delete('products/{id}/delete', [ProductsController::class, 'delete'])->middleware('auth:sanctum');
 
-
 // Brand Controller
 Route::get('brands/list/{query?}', [BrandController::class, 'read'])->middleware('auth:sanctum');
 Route::post('brands/create', [BrandController::class, 'create'])->middleware('auth:sanctum');
@@ -43,3 +45,21 @@ Route::get('category/list/{query?}', [CategoryController::class, 'read'])->middl
 Route::post('category/create', [CategoryController::class, 'create'])->middleware('auth:sanctum');
 Route::put('category/{id}/update', [CategoryController::class, 'update'])->middleware('auth:sanctum');
 Route::delete('category/{id}/delete', [CategoryController::class, 'delete'])->middleware('auth:sanctum');
+
+// Customers
+Route::get('customer/list/{query?}', [CustomerController::class, 'read'])->middleware('auth:sanctum');
+Route::post('customer/create', [CustomerController::class, 'create'])->middleware('auth:sanctum');
+Route::put('customer/{id}/update', [CustomerController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('customer/{id}/delete', [CustomerController::class, 'delete'])->middleware('auth:sanctum');
+
+// Order
+Route::get('order/list/{query?}', [OrderController::class, 'read'])->middleware('auth:sanctum');
+Route::post('order/create', [OrderController::class, 'create'])->middleware('auth:sanctum');
+Route::put('order/{id}/update', [OrderController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('order/{id}/delete', [OrderController::class, 'delete'])->middleware('auth:sanctum');
+
+// Payment
+Route::get('payment/list/{query?}', [PaymentController::class, 'read'])->middleware('auth:sanctum');
+Route::post('payment/create', [PaymentController::class, 'create'])->middleware('auth:sanctum');
+Route::put('payment/{id}/update', [PaymentController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('payment/{id}/delete', [PaymentController::class, 'delete'])->middleware('auth:sanctum');
